ecef14ea413e1413d501683c98cb77f5ab2c7287
Jakob Truelsen <jakob@scalgo.com>
2017-04-01 17:17:39 +0200

    Allow a box to have a complex title

--- frontend/src/box.tsx
+++ frontend/src/box.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
-export function Box( {title, collapsable, expanded, children}:{title:string, collapsable?:boolean, expanded?:boolean, children?:JSX.Element[]}) {
+export function Box( {title, collapsable, expanded, children}:{title:React.ReactNode, collapsable?:boolean, expanded?:boolean, children?:JSX.Element[]}) {
     return (
         <Card initiallyExpanded={!collapsable || expanded} style={{marginBottom:"20px"}}>
             <CardHeader style={{backgroundColor:"lightgray"}} titleStyle={{fontWeight:"bold", fontSize:"130%"}} title={title} actAsExpander={collapsable} showExpandableButton={collapsable}/>                         
