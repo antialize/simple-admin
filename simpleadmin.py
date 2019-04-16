923b2fc3988bf389eb632ab28bd6aea10973d8b6
Jakob Truelsen <jakob@scalgo.com>
2019-04-16 12:03:10 +0200

    Allow the cli interface to edit objects

--- simpleadmin.py
+++ simpleadmin.py
@@ -2,6 +2,7 @@
 
 import argparse, getpass, os, json, asyncio, subprocess, base64, socket, random
 import websockets
+from tempfile import NamedTemporaryFile
 
 remote = json.load(open("/etc/simpleadmin_client.json", "r"))['server_host']
 
@@ -98,6 +99,64 @@ async def deploy(server, image, container=None, config=None, restore_on_failure=
             print(res['message'])
             break
 
+async def edit(path):
+    c = Connection()
+    await c.setup(requireAuth=True)
+    ref = random.randint(0, 2**48-1)
+    await c.socket.send(json.dumps({"type": "GetObjectId", "path": path, "ref": ref}))
+
+    id = None
+    while True:
+        res = json.loads(await c.socket.recv())
+        if res['type'] == 'GetObjectIdRes' and res['ref'] == ref:
+            id = res['id']
+            break
+
+    if not id:
+        print("Object not found")
+        return
+
+    await c.socket.send(json.dumps({"type": "FetchObject", "id": id}))
+    res = None
+    while True:
+        res = json.loads(await c.socket.recv())
+        if res['type'] == 'ObjectChanged' and res['id'] == id:
+            break
+    cur = res['object'][-1]
+
+    ext = ".json"
+    path = "content"
+    if cur['type'] == 10226:
+        ext = ".txt"
+        path = 'content.content'
+
+    with NamedTemporaryFile(mode='w', suffix=ext) as f:
+        w = cur
+        for n in path.split('.')[:-1]:
+            w = w[n]
+        t = path.split('.')[-1]
+        if isinstance(w[t], str):
+            f.write(w[t])
+        else:
+            f.write(json.dumps(w[t], indent=4))
+        f.flush()
+        proc = await asyncio.create_subprocess_exec('vi', f.name)
+        if await proc.wait() != 0:
+            return
+        with open(f.name, mode="r") as f2:
+            if isinstance(w[t], str):
+                w[t] = f2.read()
+            else:
+                w[t] = json.loads(f2.read())
+
+    await c.socket.send(json.dumps({'type': 'SaveObject', 'id': id, 'obj': cur}));
+
+    while True:
+        res = json.loads(await c.socket.recv())
+        if res['type'] == 'ObjectChanged' and res['id'] == id:
+            print("Save")
+            break
+
 def main():
     parser = argparse.ArgumentParser(prog='simpleadmin')
     subparsers = parser.add_subparsers(help='commands', dest="command")
@@ -108,20 +167,27 @@ def main():
     parser_deauth = subparsers.add_parser('deauth', help='Deauthenticate user', description="Deauthenticate your user")
     parser_deauth.add_argument('-f, --full', dest='full', action='store_true', help="Forget two factor authentication")
 
-    parser_deploy = subparsers.add_parser('deploy', help='Deploy', description="Deploy image on server")
+    parser_deploy = subparsers.add_parser('dockerDeploy', help='Deploy', description="Deploy image on server")
     parser_deploy.add_argument('server', help="The server to deploy on")
     parser_deploy.add_argument('image', help="The image to deploy")
     parser_deploy.add_argument('-s, --container', dest='container', help="The container to deloy to", default=None)
     parser_deploy.add_argument('-c, --config', dest='config', help="The config to use", default=None)
     parser_deploy.add_argument('--no-restore-on-failure', dest='restore_on_failure', action='store_false')
 
+
+    parser_deploy = subparsers.add_parser('edit', help='Edit', description="Edit an object")
+    parser_deploy.add_argument('object', help="The server to deploy on")
+
     args = parser.parse_args()
     if args.command == 'auth':
         asyncio.get_event_loop().run_until_complete(auth(args.user))
     elif args.command == 'deauth':
         asyncio.get_event_loop().run_until_complete(deauth(args.full))
-    elif args.command == 'deploy':
+    elif args.command == 'dockerDelpoy':
         asyncio.get_event_loop().run_until_complete(deploy(args.server, args.image, args.container, args.config, args.restore_on_failure))
+    elif args.command == 'edit':
+        asyncio.get_event_loop().run_until_complete(edit(args.object))
+
 
 if __name__ == '__main__':
     main()
