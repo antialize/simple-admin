e542c9baab388e9a9eed8f3824acd594ed002271
Jakob Truelsen <jakob@scalgo.com>
2019-10-08 14:09:35 +0200

    Add missing dep

--- server/src/setup.ts
+++ server/src/setup.ts
@@ -46,7 +46,7 @@ export default async (req: Request, res: Response) => {
     script += "add-apt-repository universe\n";
     script += "apt update\n";
     // smartmontools recommends mailx which depends on some mail-transport-agent. Install ssmtp instead of postfix.
-    script += "apt install -y python3 python3-dbus git smartmontools postfix- ssmtp python3-websockets\n";
+    script += "apt install -y python3 python3-dbus git smartmontools postfix- ssmtp python3-websockets python3-aiohttp\n";
     script += "echo '{\"password\": \""+npw+"\", \"server_host\": \""+config.hostname+"\", \"hostname\": \""+host+"\"}' > /etc/simpleadmin_client.json\n";
     script += "rm -rf /opt/simple-admin\n";
     script += "mkdir -p /opt/simple-admin\n";
