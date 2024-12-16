8992edd47ef600ab15473e7e97330250ff1ca4de
Jakob Truelsen <jakob@scalgo.com>
2024-12-16 12:54:00 +0100

    server: Upgrade to neon 1.1 alpha

--- server-rs/src/lib.rs
+++ server-rs/src/lib.rs
@@ -1,45 +1,22 @@
-use neon::prelude::*;
 mod crypt;
+use neon::types::extract::{Error, Json};
 
-fn crypt_hash(mut cx: FunctionContext) -> JsResult<JsString> {
-    let key: Handle<JsString> = cx.argument(0)?;
-    let v = crypt::hash(&key.value(&mut cx)).or_else(|e| cx.throw_error(format!("{:?}", e)))?;
-    Ok(cx.string(v))
+#[neon::export(name = "cryptHash")]
+fn crypt_hash(key: String) -> Result<String, Error> {
+    Ok(crypt::hash(&key)?)
 }
 
-fn crypt_validate_password(mut cx: FunctionContext) -> JsResult<JsBoolean> {
-    let provided: Handle<JsString> = cx.argument(0)?;
-    let hash: Handle<JsString> = cx.argument(1)?;
-    let v = crypt::validate_password(&provided.value(&mut cx), &hash.value(&mut cx))
-        .or_else(|e| cx.throw_error(format!("{:?}", e)))?;
-    Ok(cx.boolean(v))
+#[neon::export(name = "cryptValidatePassword")]
+fn crypt_validate_password(provided: String, hash: String) -> Result<bool, Error> {
+    Ok(crypt::validate_password(&provided, &hash)?)
 }
 
-fn crypt_validate_otp(mut cx: FunctionContext) -> JsResult<JsBoolean> {
-    let token: Handle<JsString> = cx.argument(0)?;
-    let base32_secret: Handle<JsString> = cx.argument(1)?;
-    let v = crypt::validate_otp(&token.value(&mut cx), &base32_secret.value(&mut cx))
-        .or_else(|e| cx.throw_error(format!("{:?}", e)))?;
-    Ok(cx.boolean(v))
+#[neon::export(name = "cryptValidateOtp")]
+fn crypt_validate_otp(token: String, base32_secret: String) -> Result<bool, Error> {
+    Ok(crypt::validate_otp(&token, &base32_secret)?)
 }
 
-pub fn crypt_generate_otp_secret(mut cx: FunctionContext) -> JsResult<JsArray> {
-    let name: Handle<JsString> = cx.argument(0)?;
-    let (secret, url) = crypt::generate_otp_secret(name.value(&mut cx))
-        .or_else(|e| cx.throw_error(format!("{:?}", e)))?;
-    let secret = cx.string(secret);
-    let url = cx.string(url);
-    let res = cx.empty_array();
-    res.set(&mut cx, 0, secret)?;
-    res.set(&mut cx, 1, url)?;
-    Ok(res)
-}
-
-#[neon::main]
-fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("cryptHash", crypt_hash)?;
-    cx.export_function("cryptValidatePassword", crypt_validate_password)?;
-    cx.export_function("cryptValidateOtp", crypt_validate_otp)?;
-    cx.export_function("cryptGenerateOtpSecret", crypt_generate_otp_secret)?;
-    Ok(())
+#[neon::export(name = "cryptGenerateOtpSecret")]
+fn crypt_generate_otp_secret(name: String) -> Result<Json<(String, String)>, Error> {
+    Ok(Json(crypt::generate_otp_secret(name)?))
 }
