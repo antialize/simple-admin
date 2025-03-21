antialize/simple-admin

commit d53fe3457e0eeb9c93250cdf57f581b3d08bd1cb

frontend: Use generated types from rust

frontend/src/Type.tsx
@@ -9,7 +9,8 @@ import Password from "./Password";
 import Triggers from "./Triggers";
 import TypeContent from "./TypeContent";
 import Variables from "./Variables";
-import { TypePropType, hostId, rootId, typeId } from "./shared/type";
+import nullCheck from "./nullCheck";
+import { HOST_ID, ROOT_ID, TYPE_ID, TypePropType } from "./shared_types";
 import state from "./state";
 
 const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id: number }) {
@@ -30,7 +31,7 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
         obj.touched = true;
     };
     for (const ct of type?.content ?? []) {
-        if (ct.type === TypePropType.none) continue;
+        if (ct.type === TypePropType.none || ct.type === TypePropType.monitor) continue;
         const v = c[ct.name];
 
         switch (ct.type) {
@@ -123,10 +124,10 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
                         setData={(v: string) => {
                             setProp(ct.name, v);
                         }}
-                        lang={ct.lang ?? c[ct.langName]}
+                        lang={ct.lang ?? c[nullCheck(ct.langName)]}
                         fixedLang={ct.lang != null && ct.lang !== ""}
                         setLang={(v: string) => {
-                            setProp(ct.langName, v);
+                            setProp(nullCheck(ct.langName), v);
                         }}
                     />,
                 );
@@ -224,7 +225,7 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
                     >
                         <ObjectSelector
                             filter={(type, _) =>
-                                type !== hostId && type !== typeId && type !== rootId
+                                type !== HOST_ID && type !== TYPE_ID && type !== ROOT_ID
                             }
                             selected={c.contains ? c.contains : []}
                             setSelected={(sel: number[]) => {
@@ -237,7 +238,7 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
                     <InformationListRow name="Depends on" long={true} key="depends_on">
                         <ObjectSelector
                             filter={(type, _) =>
-                                type !== hostId && type !== typeId && type !== rootId
+                                type !== HOST_ID && type !== TYPE_ID && type !== ROOT_ID
                             }
                             selected={c.depends ? c.depends : []}
                             setSelected={(sel: number[]) => {
@@ -249,7 +250,7 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
                 {type.hasSudoOn ? (
                     <InformationListRow name="Sudo on" long={true} key="sudo_on">
                         <ObjectSelector
-                            filter={(type, _) => type === hostId}
+                            filter={(type, _) => type === HOST_ID}
                             selected={c.sudoOn ? c.sudoOn : []}
                             setSelected={(sel: number[]) => {
                                 setProp("sudoOn", sel);

frontend/src/ActionTargets.ts
@@ -1,15 +1,15 @@
-import type { ACTION, IAction } from "./shared/actions";
-import getOrInsert from "./shared/getOrInsert";
+import getOrInsert from "./getOrInsert";
+import type { IServerAction } from "./shared_types";
 
-type ActionTarget = (action: IAction) => boolean;
+type ActionTarget = (action: IServerAction) => boolean;
 
 export class ActionTargets {
-    targets = new Map<ACTION, Set<ActionTarget>>();
-    add(action: ACTION, target: ActionTarget): void {
+    targets = new Map<string, Set<ActionTarget>>();
+    add(action: string, target: ActionTarget): void {
         getOrInsert(this.targets, action, () => new Set()).add(target);
     }
 
-    remove(action: ACTION, target: ActionTarget): void {
+    remove(action: string, target: ActionTarget): void {
         const p = this.targets.get(action);
         if (p != null) {
             p.delete(target);

frontend/src/DockerImages.tsx
@@ -6,10 +6,8 @@ import DisplayError from "./Error";
 import InfoTable, { InfoTableHeader } from "./InfoTable";
 import UnixTime from "./UnixTime";
 import extractRemote from "./extractRemote";
-import { ACTION } from "./shared/actions";
-import nullCheck from "./shared/nullCheck";
-import * as State from "./shared/state";
-import type { IPage } from "./shared/state";
+import nullCheck from "./nullCheck";
+import { type IPage, PAGE_TYPE } from "./shared_types";
 import state from "./state";
 
 export const DockerImages = observer(function DockerImages() {
@@ -39,7 +37,7 @@ export const DockerImages = observer(function DockerImages() {
                 commit = `${tag.labels.GIT_BRANCH || ""} ${tag.labels.GIT_COMMIT || ""}`;
             }
             const historyPage: IPage = {
-                type: State.PAGE_TYPE.DockerImageHistory,
+                type: PAGE_TYPE.DockerImageHistory,
                 project: tag.image,
                 tag: tag.tag,
             };
@@ -62,7 +60,7 @@ export const DockerImages = observer(function DockerImages() {
                                 checked={!!tag.pin}
                                 onChange={(e) => {
                                     state.sendMessage({
-                                        type: ACTION.DockerImageSetPin,
+                                        type: "DockerImageSetPin",
                                         id: tag.id,
                                         pin: e.target.checked,
                                     });
@@ -74,7 +72,7 @@ export const DockerImages = observer(function DockerImages() {
                             checked={!!pin}
                             onChange={(e) => {
                                 state.sendMessage({
-                                    type: ACTION.DockerImageTagSetPin,
+                                    type: "DockerImageTagSetPin",
                                     image: project,
                                     tag: tag.tag,
                                     pin: e.target.checked,
@@ -143,8 +141,7 @@ export const DockerImageHistory = observer(function DockerImageHistory() {
     const spage = state.page;
     if (!spage) return <DisplayError>Missing state.page</DisplayError>;
     const page = spage.current;
-    if (page.type !== State.PAGE_TYPE.DockerImageHistory)
-        return <DisplayError>Wrong page</DisplayError>;
+    if (page.type !== PAGE_TYPE.DockerImageHistory) return <DisplayError>Wrong page</DisplayError>;
 
     const h1 = s.imageHistory.get(page.project);
     const r = extractRemote(h1?.get(page.tag));
@@ -179,7 +176,7 @@ export const DockerImageHistory = observer(function DockerImageHistory() {
                             checked={!!image.pin}
                             onChange={(e) => {
                                 state.sendMessage({
-                                    type: ACTION.DockerImageSetPin,
+                                    type: "DockerImageSetPin",
                                     id: image.id,
                                     pin: e.target.checked,
                                 });

frontend/src/DockerImagesState.ts
@@ -1,14 +1,13 @@
 import { action, makeObservable, observable } from "mobx";
 import type Remote from "./Remote";
-import {
-    ACTION,
-    type DockerImageTag,
-    type IDockerImageTagsCharged,
-    type IDockerListImageTagHistoryRes,
-    type IDockerListImageTagsRes,
-} from "./shared/actions";
-import getOrInsert from "./shared/getOrInsert";
-import nullCheck from "./shared/nullCheck";
+import getOrInsert from "./getOrInsert";
+import nullCheck from "./nullCheck";
+import type {
+    DockerImageTag,
+    IDockerListImageTagHistoryRes,
+    IDockerListImageTagsCharged,
+    IDockerListImageTagsRes,
+} from "./shared_types";
 import state from "./state";
 
 export default class DockerImagesState {
@@ -31,7 +30,7 @@ export default class DockerImagesState {
     load() {
         if (this.projects.state !== "initial") return;
         state.sendMessage({
-            type: ACTION.DockerListImageTags,
+            type: "DockerListImageTags",
             ref: 0,
         });
         this.projects = { state: "loading" };
@@ -52,7 +51,7 @@ export default class DockerImagesState {
         const h2 = h1.get(tag);
         if (h2 && h2.state !== "initial") return;
         state.sendMessage({
-            type: ACTION.DockerListImageTagHistory,
+            type: "DockerListImageTagHistory",
             ref: 0,
             image: project,
             tag,
@@ -81,7 +80,7 @@ export default class DockerImagesState {
     }
 
     @action
-    handleChange(act: IDockerImageTagsCharged) {
+    handleChange(act: IDockerListImageTagsCharged) {
         if (this.projects.state === "data") {
             const projects = this.projects.data;
             for (const tag of act.changed) {

frontend/src/DockerServices.tsx
@@ -6,10 +6,7 @@ import InfoTable, { InfoTableHeader } from "./InfoTable";
 import { InformationList, InformationListRow } from "./InformationList";
 import UnixTime from "./UnixTime";
 import extractRemote from "./extractRemote";
-import { ACTION } from "./shared/actions";
-import type { IPage } from "./shared/state";
-import * as State from "./shared/state";
-import { hostId } from "./shared/type";
+import { HOST_ID, type IPage, PAGE_TYPE } from "./shared_types";
 import state from "./state";
 
 export const HostDockerContainers = observer(function DockerContainers(p: {
@@ -24,7 +21,7 @@ export const HostDockerContainers = observer(function DockerContainers(p: {
     const page = state.page;
     if (!page) return <DisplayError>Missing state.page</DisplayError>;
     const hosts = r.data;
-    const hostDigests = state.objectDigests.get(hostId);
+    const hostDigests = state.objectDigests.get(HOST_ID);
     const hostDigest = hostDigests?.get(p.host);
     const hostName = hostDigest?.name;
     if (!hostName) return <DisplayError>Missing hostName</DisplayError>;
@@ -42,12 +39,12 @@ export const HostDockerContainers = observer(function DockerContainers(p: {
             commit = `${container.imageInfo.labels.GIT_BRANCH || ""} ${container.imageInfo.labels.GIT_COMMIT || ""}`;
         }
         const historyPage: IPage = {
-            type: State.PAGE_TYPE.DockerContainerHistory,
+            type: PAGE_TYPE.DockerContainerHistory,
             host: p.host,
             container: container.name,
         };
         const detailsPage: IPage = {
-            type: State.PAGE_TYPE.DockerContainerDetails,
+            type: PAGE_TYPE.DockerContainerDetails,
             host: p.host,
             container: container.name,
             id: container.id,
@@ -67,7 +64,7 @@ export const HostDockerContainers = observer(function DockerContainers(p: {
                         onClick={() => {
                             confirm("Forget this container from host?") &&
                                 state.sendMessage({
-                                    type: ACTION.DockerContainerForget,
+                                    type: "DockerContainerForget",
                                     host: p.host,
                                     container: container.name,
                                 });
@@ -162,9 +159,9 @@ export const DockerServiceDetails = observer(function DockerServiceDetails() {
     const spage = state.page;
     if (!spage) return <DisplayError>Missing state.page</DisplayError>;
     const page = spage.current;
-    if (page.type !== State.PAGE_TYPE.DockerContainerDetails)
+    if (page.type !== PAGE_TYPE.DockerContainerDetails)
         return <DisplayError>Wrong page type</DisplayError>;
-    const hosts = state.objectDigests.get(hostId);
+    const hosts = state.objectDigests.get(HOST_ID);
     const host = hosts?.get(page.host);
     const hostName = host?.name;
     if (!hostName) return <DisplayError>Missing host name</DisplayError>;
@@ -239,9 +236,9 @@ export const DockerServiceHistory = observer(function DockerServiceHistory() {
     const spage = state.page;
     if (!spage) return <DisplayError>Missing state.page</DisplayError>;
     const page = spage.current;
-    if (page.type !== State.PAGE_TYPE.DockerContainerHistory)
+    if (page.type !== PAGE_TYPE.DockerContainerHistory)
         return <DisplayError>Wrong page type</DisplayError>;
-    const hosts = state.objectDigests.get(hostId);
+    const hosts = state.objectDigests.get(HOST_ID);
     const host = hosts?.get(page.host);
     const hostName = host?.name;
     if (!hostName) return <DisplayError>Missing host name</DisplayError>;
@@ -264,7 +261,7 @@ export const DockerServiceHistory = observer(function DockerServiceHistory() {
             commit = `${container.imageInfo.labels.GIT_BRANCH || ""} ${container.imageInfo.labels.GIT_COMMIT || ""}`;
         }
         const detailsPage: IPage = {
-            type: State.PAGE_TYPE.DockerContainerDetails,
+            type: PAGE_TYPE.DockerContainerDetails,
             host: page.host,
             container: container.name,
             id: container.id,

frontend/src/DockerServicesState.ts
@@ -1,13 +1,12 @@
 import { ObservableMap, action, makeObservable, observable } from "mobx";
 import type Remote from "./Remote";
-import {
-    ACTION,
-    type DockerDeployment,
-    type IDockerDeploymentsChanged,
-    type IDockerListDeploymentHistoryRes,
-    type IDockerListDeploymentsRes,
-} from "./shared/actions";
-import getOrInsert from "./shared/getOrInsert";
+import getOrInsert from "./getOrInsert";
+import type {
+    DockerDeployment,
+    IDockerDeploymentsChanged,
+    IDockerListDeploymentHistoryRes,
+    IDockerListDeploymentsRes,
+} from "./shared_types";
 import state from "./state";
 
 export default class DockerrvicesState {
@@ -28,7 +27,7 @@ export default class DockerrvicesState {
     load() {
         if (this.hosts.state !== "initial") return;
         state.sendMessage({
-            type: ACTION.DockerListDeployments,
+            type: "DockerListDeployments",
             ref: 0,
         });
         this.hosts = { state: "loading" };
@@ -44,7 +43,7 @@ export default class DockerrvicesState {
         const c2 = c1.get(container);
         if (c2 && c2.state !== "initial") return;
         state.sendMessage({
-            type: ACTION.DockerListDeploymentHistory,
+            type: "DockerListDeploymentHistory",
             host,
             name: container,
             ref: 0,

frontend/src/HostChip.tsx
@@ -1,18 +1,17 @@
 import { Chip } from "@mui/material";
 import { observer } from "mobx-react";
-import * as State from "./shared/state";
-import { hostId } from "./shared/type";
+import { HOST_ID, type IPage, PAGE_TYPE } from "./shared_types";
 import state from "./state";
 
 const HostChip = observer(function HostChip({ id }: { id: number }) {
     const page = state.page;
     if (page === null) return <span>Missing state.page</span>;
 
-    const hosts = state.objectDigests.get(hostId);
+    const hosts = state.objectDigests.get(HOST_ID);
     const host = hosts?.get(id);
     const name = host?.name;
     const up = state.hostsUp.has(id);
-    const pageDetails: State.IPage = { type: State.PAGE_TYPE.Object, objectType: hostId, id };
+    const pageDetails: IPage = { type: PAGE_TYPE.Object, objectType: HOST_ID, id };
     return (
         <Chip
             style={{ margin: "4px" }}

frontend/src/LoginState.ts
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 import { action, makeObservable, observable } from "mobx";
-import { ACTION, type ILogin, type ILogout } from "./shared/actions";
+import type { IClientAction } from "./shared_types";
 import state, { CONNECTION_STATUS } from "./state";
 
 class LoginState {
@@ -19,8 +19,8 @@ class LoginState {
 
     @action
     login() {
-        const l: ILogin = {
-            type: ACTION.Login,
+        const l: IClientAction = {
+            type: "Login",
             user: this.user,
             pwd: this.pwd,
             otp: this.otp,
@@ -34,8 +34,8 @@ class LoginState {
 
     @action
     logout(forgetOtp: boolean) {
-        const l: ILogout = {
-            type: ACTION.Logout,
+        const l: IClientAction = {
+            type: "Logout",
             forgetPwd: true,
             forgetOtp,
         };

commit 5e1912659163e8d1f41e9b0869b379f6643e9676

Fix react render crash when langName is null
Fixes: d53fe34 ("frontend: Use generated types from rust", 2025-02-03)

frontend/src/Type.tsx
@@ -9,7 +9,6 @@ import Password from "./Password";
 import Triggers from "./Triggers";
 import TypeContent from "./TypeContent";
 import Variables from "./Variables";
-import nullCheck from "./nullCheck";
 import { HOST_ID, ROOT_ID, TYPE_ID, TypePropType } from "./shared_types";
 import state from "./state";
 
@@ -124,10 +123,10 @@ const Type = observer(function Type({ typeId: myType, id }: { typeId: number; id
                         setData={(v: string) => {
                             setProp(ct.name, v);
                         }}
-                        lang={ct.lang ?? c[nullCheck(ct.langName)]}
+                        lang={ct.lang ?? (ct.langName == null ? undefined : c[ct.langName])}
                         fixedLang={ct.lang != null && ct.lang !== ""}
                         setLang={(v: string) => {
-                            setProp(nullCheck(ct.langName), v);
+                            if (ct.langName != null) setProp(ct.langName, v);
                         }}
                     />,
                 );